4f04bc97ccba9e4c2b3511740b5499770a3ed0b7
Venkata Krishnan <110184468+pernelkanic@users.noreply.github.com>
2023-11-27 12:16:42 +0530

    Update udp_server.py

--- udp_server.py
+++ udp_server.py
@@ -1,8 +1,9 @@
 import socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-message = 'Test message. This will be echoed'
-addr = ('127.0.0.1', 12000)
-client_socket.sendto(message.encode('utf-8'),addr)
-data,server = client_socket.recvfrom(1024)
-print("received %s" % data)
-client_socket.close()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+addr=('', 12000)
+server_socket.bind(addr)
+while True:
+  print("Waiting to receive message from client")
+  message,address = server_socket.recvfrom(1024)
+  print("Received %s" %message)
+  server_socket.sendto(message,address)
