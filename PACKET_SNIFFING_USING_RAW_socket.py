049d71da08f6539d9470f57cda43080bcae7cb83
Venkata Krishnan <110184468+pernelkanic@users.noreply.github.com>
2023-11-27 11:45:37 +0530

    Add files via upload

--- PACKET_SNIFFING_USING_RAW_socket.py
+++ PACKET_SNIFFING_USING_RAW_socket.py
@@ -1,24 +1,13 @@
-import socket import
-struct import binascii
-s= socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_IP)
-s.bind(("127.0.0.1",0))
-packet=s.recvfrom(65565)
-print(packet)
-ethernet_header = packet[0][0:14]
-eth_header = struct.unpack("!6s6s2s", ethernet_header)
-print("ETHERNET HEADER")
-print("****************")
-print("Destination Address")
-print( binascii.hexlify(eth_header[0]))
-print("Source Address")
-print( binascii.hexlify(eth_header[1]))
-print("Type")
-print( binascii.hexlify(eth_header[2]))
-print("IP HEADER")
-print("*********")
-ipheader = packet[0][14:34]
-ip_header = struct.unpack("!12s4s4s", ipheader)
-print("Destination Address")
-print (socket.inet_ntoa(ip_header[1]))
-print("Source Address")
-print(socket.inet_ntoa(ip_header[2]))
+import struct
+import socket
+import binascii
+s=socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_IP)
+s.bind(('127.0.0.1',0))
+p=s.recvfrom(65565)
+print(p)
+eth_head=p[0][0:14]
+eh=struct.unpack('!6s6s2s',eth_head)
+print("ETH HEAD")
+print('des add:',binascii.hexlify(eh[0]))
+print('src add:',binascii.hexlify(eh[1]))
+print('type:',binascii.hexlify(eh[2]))
